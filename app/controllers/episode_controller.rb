7efabde7f34a5c11f9926bcd500493282981c38d
Stuart Lynn <stuart.lynn@gmail.com>
2015-09-19 21:44:52 -0400

    bug fix

---
+++ app/controllers/episode_controller.rb
@@ -0,0 +1,15 @@
+class EpisodeController < ApplicationController
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  def new
+    @episode = Episode.new 
+  end
+
+  def create
+    Episode.create(
+            params
+    )
+  end
+end
